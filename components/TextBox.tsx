807098038d1675f7bb414b869ce320cc5c600f64
unknown <ivanma@tuta.com>
2026-05-06 17:19:42 +0200

    Update TextBox.tsx

--- components/TextBox.tsx
+++ components/TextBox.tsx
@@ -1,14 +1,11 @@
 type TextBoxProps = {
   color: string;
   text: string;
-  withMargin?: boolean;
 };
 
-export default function TextBox({ color, text, withMargin }: TextBoxProps) {
+export default function TextBox({ color, text }: TextBoxProps) {
   return (
-    <div
-      className={`${withMargin ? "m-2 mt-3" : ""} px-2 w-fit h-fit border rounded-4xl ${color}`}
-    >
+    <div className={`px-2 w-fit h-fit border rounded-4xl ${color}`}>
       <h3 className="md:text-2xl">{text}</h3>
     </div>
   );
